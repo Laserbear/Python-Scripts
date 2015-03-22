04dc50b7563d1864d0f63d166290a8c45d4a5a0a
Laserbear <christianr.ng@gmail.com>
2015-03-21 18:03:37 -0700

    Create montyhall.py
    Illustration of Monty Hall problem.
    
    For simplicity's sake, Door number one was assumed to be the correct door. However, the same theory can be applied no matter which door was the correct one.
    
    Links to information and mathematical explanations of the Monty Hall Problem are available at http://en.wikipedia.org/wiki/Monty_Hall_problem

---
+++ montyhall.py
@@ -0,0 +1,47 @@
+import random
+
+class Door(object):
+	wins = 0
+	losses = 0
+	wins_Switch = 0
+	losses_Switch = 0
+	def __init__(self, type):
+		self.type = type
+	def Reveal(self):
+		if self.type == 1:
+			Door.wins = Door.wins + 1
+		else:
+			Door.losses = Door.losses + 1
+	def Reveal_Switch(self):
+		if self.type == 1:
+			Door.wins_Switch = Door.wins_Switch + 1
+		else:
+			Door.losses_Switch = Door.losses_Switch + 1
+door1 = Door(1)
+door2 = Door(2)
+door3 = Door(2)
+trials = 1000000
+for i in range(trials):
+	choice = random.randint(1,3)
+	if choice == 1:
+		door1.Reveal()
+	elif choice == 2:
+		door2.Reveal()
+	else:
+		door3.Reveal()
+
+
+	if choice == 1:
+		door2.Reveal_Switch()
+	elif choice == 2:
+		door1.Reveal_Switch()
+	elif choice == 3:
+		door1.Reveal_Switch()
+	
+print "Trials: " + str(trials)
+print "No switching wins: " + str(Door.wins)
+print "No switching losses: " + str(Door.losses)
+print "Switching wins: " + str(Door.wins_Switch)
+print "Switching losses: " + str(Door.losses_Switch)
+
+
